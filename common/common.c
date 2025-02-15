786d1754e9eb70c1f88f4c2c0f402e5753c7484c
whoami <Synaratan@gmail.com>
2025-02-15 15:17:10 +0800

    [feat]Add a function to get the current time to the common module

--- common/common.c
+++ common/common.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "time.h"
 
 void log_data(FILE* stream, unsigned char* buf, size_t len){
     size_t i = 0;
@@ -11,4 +12,10 @@ void log_data(FILE* stream, unsigned char* buf, size_t len){
     if((i+1)%16){
         fprintf(stream, "\n");
     }
+}
+
+unsigned long long get_current_time_ms() {
+    struct timespec ts;
+    timespec_get(&ts, TIME_UTC);
+    return (unsigned long long)ts.tv_sec * 1000 + ts.tv_nsec / 1000000;
 }
--- common/common.h
+++ common/common.h
@@ -5,4 +5,6 @@
 
 extern void log_data(FILE* stream, unsigned char* buf, size_t len);
 
+extern unsigned long long get_current_time_ms();
+
 #endif
